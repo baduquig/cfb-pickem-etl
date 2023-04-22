91f34f8b507cf99ea5b0a6b06f7aff13ffd2e1e0
Gabe Baduqui <baduquig@gmail.com>
2023-04-22 13:17:48 -0700

    scraping scripts wip

--- scrape.py
+++ scrape.py
@@ -1,22 +1,26 @@
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
 
 class CollegeFootballSchedule:
     """ This class contains all the methods needed to scrape and load college schedule data from ESPN into CFB_SCHEDULE_GB dataabase. """
-    def __init__(self):
+    def __init__(self, year=2023):
         self.weeks = 14
+        self.year = year
+        self.games_df = pd.DataFrame(columns=['game_date', 'away_school', 'home_school', 'game_id', 'time', 'location'])
     
     def get_school_id(self, td_html_str):
         """Method to extract TeamID from the URL in the underlying href attribute."""
-        print(td_html_str)
-        school_span = td_html_str.find('span', class_='Table__Team')
-        print(school_span)
-        href_str = school_span.find('a', href=True)['href']
+        try:
+            school_span = td_html_str.find('span', class_='Table__Team')
+            href_str = school_span.find('a', href=True)['href']
 
-        begin_idx = href_str.index('/id/') + 4
-        end_idx = href_str.rfind('/')
-        school_id = href_str[begin_idx:end_idx]
+            begin_idx = href_str.index('/id/') + 4
+            end_idx = href_str.rfind('/')
+            school_id = href_str[begin_idx:end_idx]
+        except:
+            school_id = 0
         return school_id
     
     def get_game_id(self, td_html_str):
@@ -28,17 +32,23 @@ class CollegeFootballSchedule:
     
     def get_cell_text(self, td_html_str):
         """Method to extract the innerHTML text of the child tag of a given table cell."""
-        cell_text = td_html_str.contents[0].text
+        try:
+            cell_text = td_html_str.contents[0].text
+        except:
+            cell_text = 'TBD'
         return cell_text
 
-    def scrape_full_schedule(self, year=2023):
+    def scrape_games(self):
         """ Method to scrape all college football schedule data from https://www.espn.com/college-football/schedule for a given year (default: 2023). """
-        
+        print('\nBeginning scraping games data.\n')
+
         # Iterate through each week of 15 week schedule
         for week in range(self.weeks):
             week_num = week + 1
-            espn_url = 'https://www.espn.com/college-football/schedule/_/week/' + str(week_num) + '/year/' + str(year) + '/'
+            espn_url = 'https://www.espn.com/college-football/schedule/_/week/' + str(week_num) + '/year/' + str(self.year) + '/'
             
+            print(f'  ~ Scraping data for Week {week_num}...')
+
             # Scrape HTML from HTTP request to the URL above and store in variable `soup`
             response = requests.get(espn_url)
             soup = BeautifulSoup(response.content, 'html.parser')
@@ -51,20 +61,26 @@ class CollegeFootballSchedule:
                 game_date = day.find('div', class_='Table__Title').text
                 games_table = day.find('div', class_='Table__Scroller').find('table', class_='Table').find('tbody', class_='Table__TBODY').find_all('tr')
 
-                for game in games_table:
-                    game = game.find_all('td')
+                for game_row in games_table:
+                    game = game_row.find_all('td')
 
                     away_school = self.get_school_id(game[0])
                     home_school = self.get_school_id(game[1])
                     game_id = self.get_game_id(game[2])
-                    #time = self.get_time_attr(game[2])
-                    #final_score = self.get_cell_text(game[2])
+                    final_score = ''
                     time = self.get_cell_text(game[2])
                     location = self.get_cell_text(game[5])
 
-                    print(f'Date: {game_date} | Away School: {away_school} | Home School: {home_school} | Game ID: {game_id} | Time: {time} | Location: {location}')
-        print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+                    new_game = pd.DataFrame({
+                        'game_date' : [game_date], 'away_school' : [away_school],
+                        'home_school' : [home_school], 'game_id' : [game_id],
+                        'time' : [time], final_score: [final_score], 'location' : [location]
+                    })
+                    
+                    self.games_df = pd.concat([self.games_df, new_game], ignore_index=True)        
+        print('Completed scraping games data.')
                     
 
-cfb_sched = CollegeFootballSchedule()
-cfb_sched.scrape_full_schedule(2023)
+cfb_sched = CollegeFootballSchedule(2023)
+cfb_sched.scrape_games()
+print(cfb_sched.games_df)
