aab11fb3293d3b160b32757edae56b301acd61d3
Gabe Baduqui <baduquig@gmail.com>
2023-04-29 14:00:13 -0700

    finished full_transform() method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#from web_scraping_classes.scrape_all import ScrapeAll
+import pandas as pd
 from web_scraping_classes.scrape_games import ScrapeGames
 from web_scraping_classes.scrape_schools import ScrapeSchools
 from web_scraping_classes.scrape_conferences import ScrapeConferences
@@ -16,12 +16,14 @@ def full_extract(year):
     return games_df, schools_df, conferences_df
 
 def full_transform(schools_df, old_conf_df):
-    schools_df['conference_id'] = old_conf_df.loc[old_conf_df['school_id'] == schools_df['school_id'], 'conference_id']
-    print(schools_df)
+    schools_df = pd.merge(schools_df, old_conf_df, on='school_id', how='left')
+    schools_df = schools_df.drop(['conference_name', 'division_name'], axis=1)    
+    conferences_df = old_conf_df.drop(['school_id'], axis=1)
+    return schools_df, conferences_df
 
 def full_etl(year):
     games_df, schools_df, conferences_df = full_extract(year)
-    full_transform(schools_df, conferences_df)
+    schools_df, conferences_df = full_transform(schools_df, conferences_df)
 
 
 
