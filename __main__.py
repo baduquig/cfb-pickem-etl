01a4536e569035f1600363637f3ff19ec6949f5b
Gabe Baduqui <baduquig@gmail.com>
2023-11-18 13:53:18 -0700

    initialize `year` and `weeks` arguments

--- __main__.py
+++ __main__.py
@@ -7,7 +7,7 @@ Scrape, transform and load college football schedule data from various web pages
 import etl.cfb.extract.extract as ext
 
 def main():
-    ext.full_extract()
+    ext.full_extract(year=2023, weeks=15)
 
 if __name__ == '__main__':
     main()
