14c01eda32bb485400bcab2872a2f269a7e8eeb2
Gabriel Baduqui <baduquig@gmail.com>
2023-11-18 22:25:34 -0700

    load.py WIP...

--- __main__.py
+++ __main__.py
@@ -5,9 +5,11 @@ Author: Gabe Baduqui
 Scrape, transform and load college football schedule data from various web pages
 """
 import etl.cfb.extract.extract as ext
+import etl.cfb.load.load as load
 
 def main():
     games_raw, schools_raw, locations_raw = ext.full_extract(year=2023, weeks=15)
+    load.full_load(games_raw, schools_raw, locations_raw)
 
 if __name__ == '__main__':
     main()
--- etl/cfb/load/load.py
+++ etl/cfb/load/load.py
@@ -12,29 +12,39 @@ cfb_load_logfile_path = f'./logs/cfb_load_{timestamp}.log'
 cfb_load_logfile = open(cfb_load_logfile_path, 'a')
 
 def load_csv(df, table_name):
-    """Function that loads data from a given Pandas DataFrame into a CSV file
-       Accepts `df`: Pandas DataFrame
-       Returns: n/a"""
-    print('\n~~~~ Writing DataFrame to CSV File ~~')
-    cfb_load_logfile.write('\n~~~~ Writing DataFrame to CSV File ~~\n')
+   """Function that loads data from a given Pandas DataFrame into a CSV file
+      Accepts `df`: Pandas DataFrame
+      Returns: n/a"""
+   print('\n~~~~ Writing DataFrame to CSV File ~~')
+   cfb_load_logfile.write('\n~~~~ Writing DataFrame to CSV File ~~\n')
     
-    csv_path = f'./data/{table_name}.csv'
-    df.to_csv(csv_path, index=False)
+   csv_path = f'./data/{table_name}.csv'
+   df.to_csv(csv_path, index=False)
 
 def load_json(df, table_name):
-    """Function that loads data from a given Pandas DataFrame into a JSON object
-       Accepts `df`: Pandas DataFrame
-       Returns: n/a"""
-    print('\n~~~~ Writing DataFrame to JSON Object ~~')
-    cfb_load_logfile.write('\n~~~~ Writing DataFrame to JSON Object ~~\n')
+   """Function that loads data from a given Pandas DataFrame into a JSON object
+      Accepts `df`: Pandas DataFrame
+      Returns: n/a"""
+   print('\n~~~~ Writing DataFrame to JSON Object ~~')
+   cfb_load_logfile.write('\n~~~~ Writing DataFrame to JSON Object ~~\n')
     
-    json_path = f'./data/{table_name}.json'
-    df.to_json(json_path, orient='records')
+   json_path = f'./data/{table_name}.json'
+   df.to_json(json_path, orient='records')
 
 def full_load(games_df, schools_df, locations_df):
-    """Function that calls all necessary functions to load all CFB pickem data, stored in Pandas DataFrames, into the desired desinations
-       Accepts `games_df`: Pandas DataFrame, `schools_df`: Pandas DataFrame, `locations_df`: Pandas DataFrame
-       Returns: n/a"""
-    print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nBeginning Full Load Jobs\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
-    cfb_load_logfile.write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nBeginning Full Load Jobs\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n')
+   """Function that calls all necessary functions to load all CFB pickem data, stored in Pandas DataFrames, into the desired desinations
+      Accepts `games_df`: Pandas DataFrame, `schools_df`: Pandas DataFrame, `locations_df`: Pandas DataFrame
+      Returns: n/a"""
+   print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nBeginning Full Load Jobs\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+   cfb_load_logfile.write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nBeginning Full Load Jobs\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
+   load_csv(games_df, 'games')
+   load_csv(schools_df, 'schools')
+   load_csv(locations_df, 'locations')
+   
+   load_json(games_df, 'games')
+   load_json(schools_df, 'schools')
+   load_json(locations_df, 'locations')
+
+   print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nFinished Full Load Jobs\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+   cfb_load_logfile.write('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nFinished Full Load Jobs\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
