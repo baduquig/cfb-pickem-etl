b42b6e42863b1767420064ee0b2f54450b6fe2d6
Gabe Baduqui <baduquig@gmail.com>
2023-12-07 23:50:42 -0700

    common scrape_team_page module

---
+++ etl/common/extract/scrape_team_page.py
@@ -0,0 +1,180 @@
+"""
+Pickem ETL
+Author: Gabe Baduqui
+
+Scrape all Team-specific data elements for a given Team ID
+"""
+import requests
+from bs4 import BeautifulSoup
+
+def get_logo_url(clubhouse_div: str):
+    """Function that extracts the ESPN url to a given team's PNG image logo
+       Accepts `clubhouse_div`: <div> HTML Element String
+       Returns `logo_url`: String"""
+    # Example `clubhouse_div` string: '<div class="ClubhouseHeader__Main">...</div>'
+    try:
+        team_img = clubhouse_div.find('img', class_='Logo', src=True)
+        logo_url = team_img['src']
+    except:
+        logo_url = None
+    return logo_url
+
+def get_clubhouse_header_span(clubhouse_div: str):
+    """Function that scrapes and returns the <H1> tag storing the team name and mascot
+       Accepts `clubhose_div`: <div> HTML Element String
+       Returns `header_span`: <span> HTML Element String"""
+    # Example `clubhouse_div` string: '<div class="ClubhouseHeader__Main">...</div>'
+    try:
+        header_h1 = clubhouse_div.find('h1', class_='ClubhouseHeader__Name')
+        header_span = header_h1.find('span')
+    except:
+        header_span = None
+    return header_span
+
+def get_team_name(clubhouse_div: str):
+    """Function that scrapes the team name from a given ClubhouseHeader DIV tag
+       Accepts `clubhouse_div`: <div> HTML Element String
+       Returns `team_name`: String"""
+    # Example `clubhouse_div` string: '<div class="ClubhouseHeader__Main">...</div>'
+    header_span = get_clubhouse_header_span(clubhouse_div)
+    try:
+        team_name_span = header_span.find_all('span')[0]
+        team_name = team_name_span.text
+    except:
+        team_name = None
+    return team_name
+
+def get_team_mascot(clubhouse_div: str):
+    """Function that scrapes the team name from a given ClubhouseHeader DIV tag
+       Accepts `clubhouse_div`: <div> HTML Element String
+       Returns `team_name`: String"""
+    # Example `clubhouse_div` string: '<div class="ClubhouseHeader__Main">...</div>'
+    header_span = get_clubhouse_header_span(clubhouse_div)
+    try:
+        team_mascot_span = header_span.find_all('span')[1]
+        team_mascot = team_mascot_span.text
+    except:
+        team_mascot = None
+    return team_mascot
+
+
+
+def get_conference_name(standings_section: str, year: int):
+    """Function that scrapes the header of the a given TeamStandings SECTION tag and extracts the conference name
+       Accepts `standings_section`: <section> HTML Element String
+       Returns `conference_name`: String"""
+    # Example `standings_section String: '<section class="Card TeamStandings">...</section>'`
+    try:
+        section_header = standings_section.find('div', class_='Card__Header__Title__Wrapper')
+        h3 = section_header.find('h3').text
+        conference_name = h3.replace(f'{year} ', '')
+        conference_name = h3.replace(' Standings', '')
+    except:
+        conference_name = None
+    return conference_name
+
+def get_team_standing_row(conference_standing_rows: str, team_name=None):
+    """Function that extracts the the <tr> tag storing the conference and overall records for the current team being scraped
+       Accepts `conference_standing_rows`: List of <tr> HTML Element Strings
+       Returns `team_standing_row`: <tr> HTML Element String"""
+    # Example `conference_standing_rows` list: ['<tr class="Table__TR Table__TR--sm Table__even" data-idx="n">...</tr>', ....]
+    team_standing_row = ''
+    try:
+        for row in conference_standing_rows:
+            row_anchor = row.find('td', class_='Table__TD').find('a')
+            anchor_text = row_anchor.text
+            class_name = row_anchor['class']
+            bolded_class = 'AnchorLink fw-bold'
+
+            if ((anchor_text == team_name) or (class_name == bolded_class)):
+                team_standing_row = row
+    except:
+        team_standing_row = None
+    return team_standing_row
+
+def get_record_text(record_td: str):
+    """Function that extracts the record text from a given TD tag
+       Accepts `record_td`: <td> HTML Element String
+       Returns `record`: String"""
+    # Exampe `record_td` string: '<div class="fw-bold clr-gray-01 Table__TD">...</div>'
+    try:
+        record_text = record_td.find('span').text
+    except:
+        record_text = None
+    return record_text
+
+def get_conference_record(team_standing_row: str):
+    """Function that extracts the conference record from a given TR tag
+       Accepts `team_standing_row`: <tr> HTML Element String
+       Returns `conference_record`: String"""
+    # Example `team_standing_row` string: '<div class="Table__TR Table__TR--sm Table__even">...</div>'
+    try:
+        conf_record_td = team_standing_row.find_all('td')[1]
+        conf_record = get_record_text(conf_record_td)
+    except:
+        conf_record = None
+    return conf_record
+
+def get_overall_record(team_standing_row: str):
+    """Function that extracts the overall record from a given TR tag
+       Accepts `team_standing_row`: <tr> HTML Element String
+       Returns `overall_record`: String"""
+    # Example `team_standing_row` string: '<div class="Table__TR Table__TR--sm Table__even">...</div>'
+    try:
+        overall_record_td = team_standing_row.find_all('td')[2]
+        overall_record = get_record_text(overall_record_td)
+    except:
+        overall_record = None
+    return overall_record
+
+
+def get_team_data(team_id, year, logfile):
+    """Function that scrapes the webpage of a given Team and extracts the needed data fields.
+       Accepts `team_id`: Number
+       Returns team_data: Dictionary"""
+    print(f'~~ Scraping TeamID {team_id} data')
+    logfile.write(f'~~ Scraping TeamID {team_id} data\n')
+    espn_team_url = f'https://www.espn.com/college-football/team/_/id/{team_id}'
+
+    # Scrape HTML from HTTP request to the URL above and store in variable `page_soup`
+    custom_header = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+    }
+    team_resp = requests.get(espn_team_url, headers=custom_header)
+    team_soup = BeautifulSoup(team_resp.content, 'html.parser')
+
+    # Instantiate `team_data` dictionary
+    team_data = {
+        'team_id': team_id
+    }
+
+    # Instantiate Clubhouse Container and scrape data fields
+    try:
+        clubhouse_div = team_soup.find('div', class_='ClubhouseHeader').find('div', class_='ClubhouseHeader__Main')
+        team_data['name'] = get_team_name(clubhouse_div)
+        team_data['mascot'] = get_team_mascot(clubhouse_div)
+        team_data['logo_url'] = get_logo_url(clubhouse_div)
+    except:
+        print(f'~~~~ Could not find ClubhouseHeader Container for Team ID: {team_id}')
+        logfile.write(f'~~~~ Could not find ClubhouseHeader Container for Team ID: {team_id}\n')
+
+    # Instantiate Team Standings Section and scrape data fields
+    try:
+        standings_section = team_soup.find('section', class_='TeamStandings')
+        team_data['conference_name'] = get_conference_name(standings_section, year)
+
+        standings_tables = standings_section.find('div', class_='Wrapper Card__Content').find_all('div', class_='ResponsiveTable') 
+        for standings_table in standings_tables:
+            standings_table.find('div', class_='Table__ScrollerWrapper').find('div', class_='Table__Scroller').find('table')
+            standings_rows = standings_table.find('tbody').find_all('tr')
+            if standings_rows != '' and standings_rows is not None:
+                break
+        
+        team_standing_row = get_team_standing_row(standings_rows, team_data['name'])
+        team_data['conference_record'] = get_conference_record(team_standing_row)
+        team_data['overall_record'] = get_overall_record(team_standing_row)
+    except:
+        print(f'~~~~ Could not find Team Standings Section for TeamID: {team_id}')
+        logfile.write(f'~~~~ Could not find Team Standings Section for TeamID: {team_id}\n')
+    
+    return team_data
