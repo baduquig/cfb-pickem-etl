eee89ba0d969f69454092537f177d28e8530e260
Gabriel Baduqui <baduquig@gmail.com>
2024-06-01 15:32:27 -0700

    transform_locations_data Error Handling

--- etl/transform/common/transform_locations_data.py
+++ etl/transform/common/transform_locations_data.py
@@ -10,6 +10,12 @@ def transform_stadium_capacity(stadium_capacity_raw: str, transform_logfile: obj
        Accepts `stadium_capacity_raw`: String, `transform_logfile`: File Object
        Returns `stadium_capacity`: Number"""
     transform_logfile.write(f'Transforming stadium capacity {stadium_capacity_raw} -> ')
-    stadium_capacity = int(stadium_capacity_raw.lstrip('Capacity: ').replace(',', ''))
-    transform_logfile.write(f'{stadium_capacity}\n')
+    
+    try:
+      stadium_capacity = int(stadium_capacity_raw.lstrip('Capacity: ').replace(',', ''))
+      transform_logfile.write(f'{stadium_capacity}\n')
+    except Exception as e:
+      stadium_capacity = 0
+      transform_logfile.write(f'{e}\n')
+
     return stadium_capacity
