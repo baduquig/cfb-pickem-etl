8716cca7671998fb43967b68583d414a0af0afd5
Gabriel Baduqui <baduquig@gmail.com>
2024-06-01 15:31:15 -0700

    transform_teams_data Error Handling

--- etl/transform/common/transform_teams_data.py
+++ etl/transform/common/transform_teams_data.py
@@ -11,10 +11,14 @@ def transform_conference_name(conference_name_raw: str, transform_logfile: objec
        Returns `conference_name`: String"""
     transform_logfile.write(f'Transforming conference name {conference_name_raw} -> ')
 
-    year = conference_name_raw.split(' ')[0]
-    conference_name = conference_name_raw.replace(year, '').replace('Standings', '').lstrip().rstrip()
+    try:
+        year = conference_name_raw.split(' ')[0]
+        conference_name = conference_name_raw.replace(year, '').replace('Standings', '').lstrip().rstrip()
+        transform_logfile.write(f'{conference_name}\n')
+    except Exception as e:
+        conference_name = 'TBD'
+        transform_logfile.write(f'{e}\n')
 
-    transform_logfile.write(f'{conference_name}\n')
     return conference_name
 
 def transform_record(record_raw: str, transform_logfile: object):
@@ -23,13 +27,19 @@ def transform_record(record_raw: str, transform_logfile: object):
        Returns `wins`: Number, `losses`: Number, `ties`: Number,"""
     transform_logfile.write(f'Transforming record {record_raw} -> ')
     
-    record_elements = record_raw.split('-')
-    wins = int(record_elements[0])
-    losses = int(record_elements[1])
-    if len(record_elements) > 2:
-        ties = int(record_elements[2])
-    else:
+    try:
+        record_elements = record_raw.split('-')
+        wins = int(record_elements[0])
+        losses = int(record_elements[1])
+        if len(record_elements) > 2:
+            ties = int(record_elements[2])
+        else:
+            ties = 0
+        transform_logfile.write(f'{wins}, {losses}, {ties}\n')
+    except Exception as e:
+        wins = 0
+        losses = 0
         ties = 0
+        transform_logfile.write(f'{e}\n')
 
-    transform_logfile.write(f'{wins}, {losses}, {ties}\n')
     return wins, losses, ties
